7cd8ad9ae67a7d29a6cf2bdb2813cb4f56dd8bdd
GBally <gbally@softbankrobotics.com>
2019-02-28 16:05:48 +0800

    Adjustment of the wavebender script

--- wavebender/__init__.py
+++ wavebender/__init__.py
@@ -10,7 +10,6 @@ import math
 from . import wave
 import struct
 import random
-import argparse
 from itertools import count, islice
 
 try:
@@ -28,7 +27,7 @@ except AttributeError:
 # metadata
 __author__ = 'Zach Denton'
 __author_email__ = 'zacharydenton@gmail.com'
-__version__ = '0.3'
+__version__ = '0.4'  # Modification made to fit the MorseTranslator project
 __url__ = 'http://github.com/zacharydenton/wavebender'
 __longdescr__ = '''An audio synthesis library for Python.'''
 __classifiers__ = ['Topic :: Multimedia :: Sound/Audio :: Sound Synthesis']
@@ -85,61 +84,34 @@ def compute_samples(channels, nsamples=None):
     return islice(zip(*(map(sum, zip(*channel)) for channel in channels)), nsamples)
 
 
-def write_wavefile(f, samples, nframes=None, nchannels=2, sampwidth=2, framerate=44100, bufsize=2048):
+def write_wavefile(w, samples, nframes=None, nchannels=2, sampwidth=2,
+                   framerate=44100, bufsize=2048):
     "Write samples to a wavefile."
     if nframes is None:
         nframes = 0
 
-    w = wave.open(f, 'wb')
+    # w = wave.open(f, 'wb')
     w.setparams((nchannels, sampwidth, framerate, nframes, 'NONE', 'not compressed'))
 
     max_amplitude = float(int((2 ** (sampwidth * 8)) / 2) - 1)
 
-    # split the samples into chunks (to reduce memory consumption and improve performance)
+    # split the samples into chunks (to reduce memory consumption and improve
+    # performance)
     for chunk in grouper(bufsize, samples):
-        frames = b''.join(b''.join(struct.pack('h', int(max_amplitude * sample)) for sample in channels) for channels in chunk if channels is not None)
-        w.writeframesraw(frames)
+        return b''.join(b''.join(struct.pack('h', int(max_amplitude * sample))
+                                for sample in channels) for channels in chunk if channels is not None)
+        # w.writeframesraw(frames)
     
+    # w.close()
+
+
+def init_wave(f):
+    return wave.open(f, 'wb')
+
+
+def write_frames(w, frames):
+    w.writeframesraw(frames)
+
+
+def close_wave(w):
     w.close()
-
-
-def write_pcm(f, samples, sampwidth=2, framerate=44100, bufsize=2048):
-    "Write samples as raw PCM data."
-    max_amplitude = float(int((2 ** (sampwidth * 8)) / 2) - 1)
-
-    # split the samples into chunks (to reduce memory consumption and improve performance)
-    for chunk in grouper(bufsize, samples):
-        frames = b''.join(b''.join(struct.pack('h', int(max_amplitude * sample)) for sample in channels) for channels in chunk if channels is not None)
-        f.write(frames)
-
-    f.close()
-
-
-def main():
-    parser = argparse.ArgumentParser(prog="wavebender")
-    parser.add_argument('-c', '--channels', help="Number of channels to produce", default=2, type=int)
-    parser.add_argument('-b', '--bits', help="Number of bits in each sample", choices=(16,), default=16, type=int)
-    parser.add_argument('-r', '--rate', help="Sample rate in Hz", default=44100, type=int)
-    parser.add_argument('-t', '--time', help="Duration of the wave in "
-                                             "seconds.", default=5, type=int)
-    parser.add_argument('-a', '--amplitude', help="Amplitude of the wave on a scale of 0.0-1.0.", default=0.5, type=float)
-    parser.add_argument('-f', '--frequency', help="Frequency of the wave in Hz", default=440.0, type=float)
-    parser.add_argument('filename', help="The file to generate.")
-    args = parser.parse_args()
-
-    # each channel is defined by infinite functions which are added to produce a sample.
-    channels = ((sine_wave(args.frequency, args.rate, args.amplitude),) for i in range(args.channels))
-
-    # convert the channel functions into waveforms
-    samples = compute_samples(channels, args.rate * args.time)
-
-    # write the samples to a file
-    if args.filename == '-':
-        filename = stdout
-    else:
-        filename = args.filename
-    write_wavefile(filename, samples, args.rate * args.time, args.channels, args.bits // 8, args.rate)
-
-
-if __name__ == "__main__":
-    main()
